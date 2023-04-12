6b0c43b83e3ef256673192ef559f45ffd1c8f03e
IT20230692 <IT20230692@my.sliit.lk>
2023-04-12 19:52:30 +0530

    add jsx and scss for suppliers side

---
+++ Client/src/pages/login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Login.scss"
+
+function login() {
+  return (
+    <div className='login'>login</div>
+  )
+}
+
+export default login
---
+++ Client/src/pages/supplier/myProducts/MyProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MyProducts.scss"
+
+function MyProducts() {
+  return (
+    <div className='myproducts'>MyProducts</div>
+  )
+}
+
+export default MyProducts
