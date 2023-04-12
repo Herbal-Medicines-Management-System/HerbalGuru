85ba55d95ac44a8cead369349c3ec3ba136f5bc2
IT20137250 <it20137250@my.sliit.lk>
2023-04-13 00:18:19 +0530

    Update Folder Structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,43 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-import Products from "./pages/products/products";
+import Category from "./pages/Category/Category";
 import Product from "./pages/product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home />
-  },
-  {
-    path: "/products/:id",
-    element:<Products />
-  },
-  {
-    path: "/product/:id",
-    element:<Product />
+    element:<Layout />,
+    children: [
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/category/:id",
+        element:<Category />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      },
+    ]
   },
 ])
 
