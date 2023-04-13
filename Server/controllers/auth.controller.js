d4f38513a444bd939d1fb6d0f16f46608a29f3c4
IT20230692 <IT20230692@my.sliit.lk>
2023-04-13 18:18:19 +0530

    create supplier controllers and routes

---
+++ Server/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import User from '../models/user.model.js';
+export const register = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send('User has been created Successfully....');
+  } catch (err) {
+    res.status(500).send('Something went wrong!');
+  }
+};
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {};
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import addRoute from './routes/add.route.js';
+import orderRoute from './routes/order.route.js';
+import conversationRoute from './routes/conversation.route.js';
+import messageRoute from './routes/message.route.js';
+import reviewRoute from './routes/review.route.js';
+import authRoute from './routes/auth.route.js';
 
 const app = express();
 dotenv.config();
@@ -17,10 +23,17 @@ const connect = async () => {
   }
 };
 
-app.use("/api/users", userRoute);
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/adds', addRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/reviews', reviewRoute);
 
 app.listen(8800, () => {
   connect();
   console.log('Backend Server is running');
 });
- 
