156914d89167ef0d656515b54b203f07b9231dda
IT20137250 <it20137250@my.sliit.lk>
2023-04-13 14:45:17 +0530

    add slider

---
+++ Client/src/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import "./Slider.scss";
+
+const Slider = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? 4 : (prev) => prev - 1);
+    }
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === 4 ? 0 : (prev) => prev + 1);
+    }
+
+    return (
+        <div className='slider'>
+            <div className='container' style={{ transform: `translateX(-${currentSlide * 100}vw)`}}>
+                <img src="/img/slider1.jpg" alt="" />
+                <img src="/img/slider2.jpg" alt="" />
+                <img src="/img/slider3.jpg" alt="" />
+                <img src="/img/slider4.jpg" alt="" />
+                <img src="/img/slider5.jpg" alt="" />
+            </div>
+            <div className='icons'>
+                <div className='icon' onClick={prevSlide}>
+                    <WestOutlinedIcon />
+                </div>
+                <div className='icon' onClick={nextSlide}>
+                    <EastOutlinedIcon />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slider
