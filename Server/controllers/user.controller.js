514eb512bff7380cb14e0ce8a29da7157e169371
IT20230692 <IT20230692@my.sliit.lk>
2023-04-13 21:42:29 +0530

    create delete user with jwt middleware

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,3 +1,11 @@
-export const deleteUser = (req, res) => {
-    res.send("from controller");
-}
+import User from '../models/user.model.js';
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId != user._id.toString()) {
+    return res.status(404).send('You can delete only your account!');
+  }
+  await User.findByIdAndDelete(req.params.id);
+  req.status(200).send('Deleted successfully!');
+};
