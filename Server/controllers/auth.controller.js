bfedba088ffc7e0a64c0b2873bea9af8a5fc1cde
IT20230692 <IT20230692@my.sliit.lk>
2023-04-13 19:14:22 +0530

    add jwt token and cookies  for login

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   try {
@@ -15,6 +16,7 @@ export const register = async (req, res) => {
     res.status(500).send('Something went wrong!');
   }
 };
+
 export const login = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
@@ -23,10 +25,24 @@ export const login = async (req, res) => {
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(400).send('Wrong username or password!');
 
+    //create json web token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
     const { passwod, ...info } = user._doc;
-    res.status(200).send(user);
+    res
+      .cookie('accessToken', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (err) {
     res.status(500).send('Something went wrong!');
   }
 };
+
 export const logout = async (req, res) => {};
