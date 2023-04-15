5d286bccca1184cc19155be862050341b3f62479
IT20230692 <IT20230692@my.sliit.lk>
2023-04-15 16:45:02 +0530

    create adds delete function in server side

--- Server/controllers/add.controller.js
+++ Server/controllers/add.controller.js
@@ -1,7 +1,6 @@
 import Add from '../models/add.model.js';
 import createError from '../utils/createError.js';
 
-
 //create a new add for sellers
 export const createAdd = async (req, res, next) => {
   if (!req.isSeller)
@@ -18,7 +17,31 @@ export const createAdd = async (req, res, next) => {
     next(err);
   }
 };
-export const deleteAdd = async (req, res, next) => {};
-export const getAdd = async (req, res, next) => {};
-export const getAdds = async (req, res, next) => {};
- 
+
+//create delete function for sellers' adds
+export const deleteAdd = async (req, res, next) => {
+  try {
+    const add = await Add.findById(req.params.id);
+
+    if (add.userId !== req.userId)
+      return next(createError(403, 'You can Delete your Adds only!'));
+    await Add.findByIdAndDelete(req.params.id);
+    res.request.status(200).send('Add has been deleted!');
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAdd = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAdds = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
