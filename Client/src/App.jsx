7d9d24d784bd47d6f58fec04cc7561bdb22fc59c
IT20230692 <IT20230692@my.sliit.lk>
2023-04-19 20:29:51 +0530

    create order client side

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,67 +1,70 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
-import Home from "./pages/home/Home";
-import Category from "./pages/Category/Category";
-import Product from "./pages/product/Product";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import Home from './pages/home/Home';
+import Category from './pages/Category/Category';
+import Product from './pages/product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import MyProducts from './pages/supplier/myProducts/MyProducts';
+import Orders from './pages/supplier/orders/Orders';
 
-import "./app.scss";
+import './app.scss';
 
 const Layout = () => {
   return (
-    <div className="app">
+    <div className='app'>
       <Navbar />
       <Outlet />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
   {
-    
-    path:"/",
-    element:<Layout />,
+    path: '/',
+    element: <Layout />,
     children: [
       {
-        path:"/",
-        element:<Home />
+        path: '/',
+        element: <Home />,
       },
       {
-        path:"/category/:id",
-        element:<Category />
+        path: '/category/:id',
+        element: <Category />,
       },
       {
-        path: "/product/:id",
-        element: <Product />
+        path: '/product/:id',
+        element: <Product />,
       },
       {
-        path:"/login",
-        element:<Login />
+        path: '/login',
+        element: <Login />,
       },
       {
-        path:"/register",
-        element:<Register />
+        path: '/register',
+        element: <Register />,
       },
-    ]
-    
+      {
+        path: '/supplier/myproducts',
+        element: <MyProducts />,
+      },
+      {
+        path: '/supplier/orders',
+        element: <Orders />,
+      },
+    ],
   },
-])
+]);
 
 function App() {
-
   return (
     <div>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/src/pages/supplier/orders/Orders.jsx
+++ Client/src/pages/supplier/orders/Orders.jsx
@@ -1,10 +1,118 @@
-import React from 'react'
-import "./Orders.scss"
+import React from 'react';
+import './Orders.scss';
 
 function Orders() {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
   return (
-    <div className='orders'>Orders</div>
-  )
+    <div className='orders'>
+      <div className='container'>
+        <div className='title'>
+          <h1>Orders</h1>
+          {/* <Link to="/supplier/myproducts"> */}
+          <button>Add New Product</button>
+          {/* </Link> */}
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price(LKR)</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className='img'
+                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
+                alt='item img'
+              />
+            </td>
+            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
+            <td>100.00</td>
+            <td>20</td>
+            <td>
+              <img
+                className='contactIcon'
+                src='../../../../public/imgIcons/contact.png'
+                alt=''
+              />
+              {/* <DeleteOutlinedIcon /> */}
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className='img'
+                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
+                alt='item img'
+              />
+            </td>
+            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
+            <td>100.00</td>
+            <td>20</td>
+            <td>
+              <img
+                className='contactIcon'
+                src='../../../../public/imgIcons/contact.png'
+                alt=''
+              />
+              {/* <DeleteOutlinedIcon /> */}
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className='img'
+                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
+                alt='item img'
+              />
+            </td>
+            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
+            <td>100.00</td>
+            <td>20</td>
+            <td>
+              <img
+                className='contactIcon'
+                src='../../../../public/imgIcons/contact.png'
+                alt=''
+              />
+              {/* <DeleteOutlinedIcon /> */}
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img
+                className='img'
+                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
+                alt='item img'
+              />
+            </td>
+            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
+            <td>100.00</td>
+            <td>20</td>
+            <td>
+              <img
+                className='contactIcon'
+                src='../../../../public/imgIcons/contact.png'
+                alt=''
+              />
+              {/* <DeleteOutlinedIcon /> */}
+            </td>
+          </tr>
+
+    
+
+        
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
