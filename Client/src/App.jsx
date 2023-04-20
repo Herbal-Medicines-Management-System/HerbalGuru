d23bc8f7acac659c79a392107aad0fe134759812
IT20230692 <IT20230692@my.sliit.lk>
2023-04-20 14:00:49 +0530

    create myProducts and link server side

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,57 +9,66 @@ import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import MyProducts from './pages/supplier/myProducts/MyProducts';
 import Orders from './pages/supplier/orders/Orders';
-
+import Add from './pages/supplier/add/Add';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './app.scss';
 
-const Layout = () => {
-  return (
-    <div className='app'>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/category/:id',
-        element: <Category />,
-      },
-      {
-        path: '/product/:id',
-        element: <Product />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/register',
-        element: <Register />,
-      },
-      {
-        path: '/supplier/myproducts',
-        element: <MyProducts />,
-      },
-      {
-        path: '/supplier/orders',
-        element: <Orders />,
-      },
-    ],
-  },
-]);
 
 function App() {
+  const queryClient = new QueryClient();
+  const Layout = () => {
+    return (
+      <div className='app'>
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </QueryClientProvider>
+      </div>
+    );
+  };
+  
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/category/:id',
+          element: <Category />,
+        },
+        {
+          path: '/product/:id',
+          element: <Product />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/register',
+          element: <Register />,
+        },
+        {
+          path: '/supplier/myproducts',
+          element: <MyProducts />,
+        },
+        {
+          path: '/supplier/orders',
+          element: <Orders />,
+        },
+        {
+          path: '/supplier/add',
+          element: <Add />,
+        },
+      ],
+    },
+  ]);
+  
   return (
     <div>
       <RouterProvider router={router} />
--- Client/src/pages/supplier/myProducts/MyProducts.jsx
+++ Client/src/pages/supplier/myProducts/MyProducts.jsx
@@ -1,76 +1,85 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from 'react-router-dom';
 import './MyProducts.scss';
+import getCurrentUser from '../../../utils/getCurrentUser';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import newRequest from '../../../utils/newRequest';
 
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+// import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 function MyProducts() {
+  const currentUser = getCurrentUser();
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['myProducts'],
+    queryFn: () =>
+      newRequest.get(`/adds?userId=${currentUser.id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/adds/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['myProducts']);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className='myProducts'>
-      <div className='container'>
-        <div className='title'>
-          <h1>My Products</h1>
-          {/* <Link to="/supplier/myproducts"> */}
-           <button>Add New Product</button>
-           {/* </Link> */}
+      {isLoading ? (
+        'loading'
+      ) : error ? (
+        'error'
+      ) : (
+        <div className='container'>
+          <div className='title'>
+            <h1>My Products</h1>
+            {/* <Link to="/supplier/myproducts"> */}
+            <button>Add New Product</button>
+            {/* </Link> */}
+          </div>
+          <table>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price(LKR)</th>
+              <th>Sales</th>
+              <th>Action</th>
+            </tr>
+
+          { data.map(add=>( 
+            <tr key={add._id}>
+              <td>
+                <img
+                  className='img'
+                  src={add.cover}
+                   alt='item img'
+                />
+              </td>
+              <td>{add.title}</td>
+              <td>{add.price}</td>
+              <td>{add.sales}</td>
+              <td>
+                <img
+                  className='deleteIcon'
+                  src='../../../../public/imgIcons/delete-bin-5-line.png'
+                  alt=''
+                  onClick={() => handleDelete(add._id)}
+                />
+              </td>
+            </tr>))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price(LKR)</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>
-              <img className='img'
-                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
-                alt='item img'
-              />
-            </td>
-            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
-            <td>100.00</td>
-            <td>20</td>
-            <td>
-              <img className='deleteIcon' src='../../../../public/imgIcons/delete-bin-5-line.png' alt='' />
-              {/* <DeleteOutlinedIcon /> */}
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <img className='img'
-                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
-                alt='item img'
-              />
-            </td>
-            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
-            <td>100.00</td>
-            <td>20</td>
-            <td>
-              <img className='deleteIcon' src='../../../../public/imgIcons/delete-bin-5-line.png' alt='' />
-              {/* <DeleteOutlinedIcon /> */}
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <img className='img'
-                src='https://www.luluhypermarket.com/cdn-cgi/image/f=auto/medias/1200Wx1200H-null?context=bWFzdGVyfGltYWdlc3w0NjQ2NDl8aW1hZ2UvanBlZ3xhREZoTDJnMllTOHhOVFl4TURVd01UWXlOemt6TkM4eE1qQXdWM2d4TWpBd1NGOXVkV3hzfGJkYmQxYTIyZDBmYTIyYTk4ZDJmN2FhYTMwYzlmYWMzNGFjY2NmMTg5OWNhN2YzNDIzMjFjNTcxOWQ2YzA4MjM'
-                alt='item img'
-              />
-            </td>
-            <td>Herbal Essences Bio:Renew Hydrate Coconut Milk</td>
-            <td>100.00</td>
-            <td>20</td>
-            <td>
-              <img className='deleteIcon' src='../../../../public/imgIcons/delete-bin-5-line.png' alt='' />
-              {/* <DeleteOutlinedIcon /> */}
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 }
