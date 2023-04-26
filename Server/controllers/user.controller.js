2ad190805598fb297b55392cc200dd3b563a0787
IT20230692 <IT20230692@my.sliit.lk>
2023-04-26 23:38:37 +0530

    edit nav bar

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,17 +1,44 @@
-import User from "../models/user.model.js";
-import createError from "../utils/createError.js";
+import User from '../models/user.model.js';
+import createError from '../utils/createError.js';
 
 export const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
   if (req.userId !== user._id.toString()) {
-    return next(createError(403, "You can delete only your account!"));
+    return next(createError(403, 'You can delete only your account!'));
   }
   await User.findByIdAndDelete(req.params.id);
-  res.status(200).send("Deleted successfully!.");
+  res.status(200).send('Deleted successfully!.');
 };
+
 export const getUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
   res.status(200).send(user);
-};  
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    
+    // Find user by ID and update details
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    return res.json({ message: 'User details updated successfully', user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Server error' });
+  }
+};
+
+
