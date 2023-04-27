eea05af898069dab65d8f7f2399598cc13da8a23
IT20137250 <it20137250@my.sliit.lk>
2023-04-27 14:17:18 +0530

    add grocery page

--- src/components/TrendingProducts/TrendingProducts.jsx
+++ src/components/TrendingProducts/TrendingProducts.jsx
@@ -14,7 +14,10 @@ const TrendingProducts = ({ type }) => {
             <div className='top'>
                 <h1>{type} products</h1>
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    Ashwagandha - an adaptogenic herb used in Ayurvedic medicine to help reduce stress, anxiety, and fatigue. It is believed to promote relaxation, improve brain function, and boost immunity.
+                    Turmeric - a spice commonly used in Ayurvedic medicine for its anti-inflammatory and antioxidant properties. It has gained popularity as a natural remedy for conditions such as arthritis, heart disease, and digestive issues.
+                    Triphala - a blend of three fruits (amla, haritaki, and bibhitaki) used in Ayurvedic medicine to support digestion, detoxification, and overall health.
+                    Holy Basil (Tulsi) - an herb considered sacred in Ayurveda, used to promote respiratory health, reduce stress, and support the immune system.
                 </p>
             </div>
             <div className='bottom'>
