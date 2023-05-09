3a0bd8434797f4bbf1f431583aebc7d9a53b9b5a
IT20230692 <IT20230692@my.sliit.lk>
2023-05-10 01:59:29 +0530

    create review service

---
+++ Services/review-service/controllers/review.controller.js
@@ -0,0 +1,54 @@
+import createError from '../utils/createError.js';
+import Review from '../models/review.model.js';
+import Add from '../models/add.model.js';
+
+export const createReview = async (req, res, next) => {
+  console.log('create review');
+  if (req.isSeller)
+    return next(createError(403, "Sellers can't create a review!"));
+
+  const newReview = new Review({
+    userId: req.userId,
+    addId: req.body.addId,
+    desc: req.body.desc,
+    star: req.body.star,
+  });
+
+  try {
+    const review = await Review.findOne({
+      addId: req.body.addId,
+      userId: req.userId,
+    });
+
+    if (review)
+      return next(
+        createError(403, 'You have already created a review for this add!')
+      );
+
+    //TODO: check if the user purchased the add.
+
+    const savedReview = await newReview.save();
+
+    await Add.findByIdAndUpdate(req.body.addId, {
+      $inc: { totalStars: req.body.star, starNumber: 1 },
+    });
+    res.status(201).send(savedReview);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getReviews = async (req, res, next) => {
+  try {
+    const reviews = await Review.find({ addId: req.params.addId });
+    res.status(200).send(reviews);
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteReview = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ Services/review-service/routes/review.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { verifyToken } from '../middleware/jwt.js';
+import {
+  createReview,
+  getReviews,
+  deleteReview,
+} from '../controllers/review.controller.js';
+
+const router = express.Router();
+
+//create new review route
+router.post('/', verifyToken, createReview);
+
+//view all review relavernt addId route
+router.get('/:addId', getReviews);
+
+//delete revire route
+router.delete('/:id', deleteReview);
+
+export default router;
