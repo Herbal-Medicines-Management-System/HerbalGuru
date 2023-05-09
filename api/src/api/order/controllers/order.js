c35f5d8da7a6e286f378e29e7969ad62471f7e73
IT20230692 <IT20230692@my.sliit.lk>
2023-05-10 01:56:26 +0530

    add api

---
+++ api/src/api/order/controllers/order.js
@@ -0,0 +1,52 @@
+("use strict");
+const stripe = require("stripe")("sk_test_51MM6lbLsIqaYjsFS5jklzWVEU8V4mjewDcg2gakDsED5xU2VCGsidpS85rpk4z1X0io6CxaQIh6dehmrtY8UHbrE008J4mgjnt");
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+    async create(ctx) {
+        const { products } = ctx.request.body;
+        try {
+            const lineItems = await Promise.all(
+                products.map(async (product) => {
+                    const item = await strapi
+                        .service("api::product.product")
+                        .findOne(product.id);
+
+                    return {
+                        price_data: {
+                            currency: "LKR",
+                            product_data: {
+                                name: item.title,
+                                image: item.img,
+                            },
+                            unit_amount: Math.round(item.price * 100),
+                        },
+                        quantity: product.quantity,
+                    };
+                })
+            );
+
+            const session = await stripe.checkout.sessions.create({
+                shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+                payment_method_types: ["card"],
+                mode: "payment",
+                success_url: "http://localhost:5173/success/" + "?success=true",
+                cancel_url: "http://localhost:3000" + "?success=false",
+                line_items: lineItems,
+            });
+
+            await strapi
+                .service("api::order.order")
+                .create({ data: { products, stripeId: session.id } });
+
+            return { stripeSession: session };
+        } catch (error) {
+            ctx.response.status = 500;
+            return { error };
+        }
+    },
+}));
