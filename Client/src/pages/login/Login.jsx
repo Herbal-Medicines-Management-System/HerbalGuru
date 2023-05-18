3a622d61f9de04e7b95745e5bb6e463757837b7d
IT20230692 <IT20230692@my.sliit.lk>
2023-05-18 12:14:11 +0530

    update  with Formik and Yup validation loginjsx

--- Client/src/pages/login/Login.jsx
+++ Client/src/pages/login/Login.jsx
@@ -1,54 +1,73 @@
-import React, { useState } from "react";
-import "./Login.scss";
-import newRequest from "../../utils/newRequest";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import newRequest from '../../utils/newRequest';
+import './Login.scss';
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleFormSubmit = async (values, { setSubmitting }) => {
     try {
-      const res = await newRequest.post("/auth/login", { username, password });
-      console.log(res.data);
-      localStorage.setItem("token", res.data.token);
-      localStorage.setItem("currentUser", JSON.stringify(res.data.info));
-      navigate("/");
+      const res = await newRequest.post('/auth/login', values);
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('currentUser', JSON.stringify(res.data.info));
+      navigate('/');
     } catch (err) {
       setError(err.response.data);
+      toast.error('Login failed. Please try again.');
     }
+    setSubmitting(false);
   };
 
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit}>
-        <h1>Sign in</h1>
-        <label htmlFor="">Username</label>
-        <input
-          name="username"
-          type="text"
-          placeholder="Chandima Sooriyaarachchi"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
+    <div className='login'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={handleFormSubmit}
+      >
+        <Form>
+          <h1>Sign in</h1>
 
-        <label htmlFor="">Password</label>
-        <input
-          name="password"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+          <div>
+            <label htmlFor='username'>Username</label>
+            <Field
+              type='text'
+              id='username'
+              name='username'
+              placeholder='Chandima Sooriyaarachchi'
+            />
+            <ErrorMessage name='username' component='div' className='error' />
+          </div>
 
-        <button type="submit">Login</button>
-        {error && error}
-      </form>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <Field type='password' id='password' name='password' />
+            <ErrorMessage name='password' component='div' className='error' />
+          </div>
+
+          <button
+            type='submit'
+            style={{ display: 'flex', justifyContent: 'center' }}
+          >
+            Login
+          </button>
+
+          {error && <div className='error-message'>{error}</div>}
+        </Form>
+      </Formik>
+      <ToastContainer />
     </div>
   );
 }
